bb81128355133322971be951d95ef62bd90adeda
allen-bui <allen.vbui@gmail.com>
2019-05-05 17:07:01 -0700

    complete 'tour-this-home' button

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -6,8 +6,7 @@ import Logo from './Sub-Components/Logo.jsx';
 import Text from './Sub-Components/Text.jsx';
 import Image from './Sub-Components/Image.jsx';
 import HeartButton from './Sub-Components/HeartButton.jsx';
-
-const tourIcon = "M 3 7.5 a 1.5 1.5 0 1 1 -3 0 a 1.5 1.5 0 0 1 3 0 Z m 12.75 0 a 0.75 0.75 0 0 1 -0.75 0.75 H 6 a 0.75 0.75 0 0 1 0 -1.5 h 9 a 0.75 0.75 0 0 1 0.75 0.75 Z m -10.5 -6 A 0.75 0.75 0 0 1 6 0.75 h 9 a 0.75 0.75 0 0 1 0 1.5 H 6 a 0.75 0.75 0 0 1 -0.75 -0.75 Z m 10.5 12 a 0.75 0.75 0 0 1 -0.75 0.75 H 6 a 0.75 0.75 0 0 1 0 -1.5 h 9 a 0.75 0.75 0 0 1 0.75 0.75 Z M 3 1.5 a 1.5 1.5 0 1 1 -2.999 0.001 A 1.5 1.5 0 0 1 3 1.5 Z m 0 12 a 1.5 1.5 0 1 1 -3 0 a 1.5 1.5 0 0 1 3 0 Z"
+import TourHomeButton from './Sub-Components/TourHomeButton.jsx';
 
 const Splash = () => {
   return (
@@ -22,16 +21,7 @@ const Splash = () => {
       <div className="right-splash">
         <Image />
         <HeartButton />
-        <div className="tour-button-div">
-          <button className="tour-button">
-            <div>
-              <svg viewBox="0 0 16 16" className="tour-icon">
-                <path d={tourIcon}/>
-              </svg>
-            </div>
-          </button>
-
-        </div>
+        <TourHomeButton />
       </div>
     </div>
   );
