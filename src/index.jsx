ababc3f9deab5d0c18651d3569e7a8aad77905e3
allen-bui <allen.vbui@gmail.com>
2019-05-06 23:17:15 -0700

    Complete description service

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom';
 // Components
 import Navigation from './components/Navigation/Navigation.jsx';
 import Splash from './components/Splash/Splash.jsx';
+import Description from './components/Description/Description.jsx';
 
 const App = () => {
   return (
     <div>
-      <Navigation/>
-      <Splash/>
+      <Navigation />
+      <Splash />
+      <Description />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
